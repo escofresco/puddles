d9e678ad8652e68c676c4b0885b192761c15d596
Jonasz Karwacki <jonaszakr@gmail.com>
2020-03-24 13:53:21 -0700

    use class-based views for visitor-facing pages

--- splash/views.py
+++ splash/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+class HomeView(TemplateView):
+    template_name = "pages/home.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = "Puddles"
+        return context
+
+class PrivacyPolicyView(TemplateView):
+    template_name = "pages/privacy_policy.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = "Privacy Policy"
+        return context
+
+class TermsOfServiceView(TemplateView):
+    template_name = "pages/terms_of_service.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = "Terms of Service"
+        return context
