2ca784eb31e959c0baa2881f7427b46c075ab622
Jonasz Karwacki <jonaszakr@gmail.com>
2020-03-25 13:09:19 -0700

    remove bucket name attr

--- config/settings/production.py
+++ config/settings/production.py
@@ -102,7 +102,6 @@ from storages.backends.s3boto3 import S3Boto3Storage  # noqa E402
 class StaticRootS3Boto3Storage(S3Boto3Storage):
     location = "static"
     default_acl = "public-read"
-    bucket_name = "puddles-web-static"
 
 
 class MediaRootS3Boto3Storage(S3Boto3Storage):
