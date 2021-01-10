e6e69176ecc31d835677e2e73be9640aab40468a
SamirIngley <samir.ingle7@gmail.com>
2021-01-10 01:46:31 -0800

    imports for decision tree

--- puddleify/model_puddles.py
+++ puddleify/model_puddles.py
@@ -4,3 +4,9 @@
 
 # We'll use sci-kit learn's Decision Tree / Random Forests to classify the puddles
 
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.tree import DecisionTreeClassifier, plot_tree
+from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.metrics import confusion_matrix, plot_confusion_matrix
