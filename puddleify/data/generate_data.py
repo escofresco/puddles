7e2b2ef512ecf6f1625c27fc523e8ee3488ab250
SamirIngley <samir.ingle7@gmail.com>
2021-01-11 18:36:55 -0800

    bits and pieces from jupyter

--- puddleify/data/generate_data.py
+++ puddleify/data/generate_data.py
@@ -5,24 +5,100 @@
 
 # the dataset will have 12 attributes per item
 
+import pandas
+import random
+import string
+import decimal
+import datetime
+
 class Dataset:
 
     def __init__(self, rows, columns):
         self.rows = rows
         self.columns = columns
-        self.dataset = None
+        self.features = ["amount", "category", "date", "name", "orginal_description", "address", "city", "lat", "lon", "state", "zip"]
+        self.dataset = []
 
-    def create_empties():
-        ''' generate an empty dataset of rows r and columns c '''
-        empty_array = [[0]*self.columns]*self.rows
-        self.dataset = empty_array
-        print(self.dataset)
-        # for index, item in enumerate(self.dataset):
-        #     print(index, item)
-        return self.dataset
+    def rand_amount(self):
+        ''' generates a random amount between $2 and $250 '''
+        amount = float(decimal.Decimal(random.randrange(200, 25000))/100)
+        return amount
+    
+    def rand_category(self):
+        ''' generates random categories '''
+        categories = ['Toy', 'Grocery', 'Furniture', 'Auto', 'Loan', 'Shops', 'Computer Electronics', 'Food', 'Home Goods', 'Candy', 'Laundry', 'Rent']
+        category = random.choice(categories)
+        return category
+    
+    def rand_date(self):
+        start = datetime.datetime.now()
+        end = start + datetime.timedelta(days=365)
+        random_date = start + (end - start) * random.random()
+        return random_date
+    
+    def rand_name(self):
+        names = ["Safeway", "Albertsons", "Kroger's", "Ralphs", "Best Buy", "Micro Center", "Pizza My Heart", "Veggie Grill", "Car Wash Classic", "7 Eleven"]
+        name = random.choice(names)
+        return name
+    
+    def rand_description(self):
+        description = ''.join(random.choice(string.ascii_letters) for _ in range(10))
+        return description
+    
+    def rand_address(self):
+        we_just_gonna_use_one = "555 Post Street"
+        return we_just_gonna_use_one
+    
+    def rand_city(self):
+        just_one = "San Francisco"
+        return just_one
+    
+    def rand_lat(self):
+        return 15
+    
+    def rand_lon(self):
+        return 10
+    
+    def rand_state(self):
+        state = "CA"
+        return state
+    
+    def rand_zip(self):
+        return 94103
+    
+    def fill_dataset(self):
+    
+        for j in range(self.rows):
+            for i in range(self.columns):
+                the_row = []
+                the_row.append(self.rand_amount())
+                the_row.append(self.rand_category())
+                the_row.append(self.rand_date())
+                the_row.append(self.rand_name())
+                the_row.append(self.rand_description())
+                the_row.append(self.rand_address())
+                the_row.append(self.rand_city())
+                the_row.append(self.rand_lat())
+                the_row.append(self.rand_lon())
+                the_row.append(self.rand_zip())
+                the_row.append(self.rand_state())
+                self.dataset.append(the_row)
 
-
-
-
-if __name__ == '__main__':
-    empties(10, 12)     
+        for index, item in enumerate(self.dataset):
+            print(index, item)
+        return
+             
+    def dataset_to_csv(self, name):
+        ''' save dataset to a csv '''
+        if len(self.dataset) < 1:
+            print("create dataset")
+            return
+        df = pd.DataFrame(self.dataset)
+        df.to_csv(name+'.csv')
+        return    
+    
+        
+        
+        
+        
+        
--- puddleify/model_puddles.py
+++ puddleify/model_puddles.py
@@ -3,10 +3,44 @@
 # Test models and accuracy
 
 # We'll use sci-kit learn's Decision Tree / Random Forests to classify the puddles
-
+import json 
+import csv 
+  
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.tree import DecisionTreeClassifier, plot_tree
 from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn.metrics import confusion_matrix, plot_confusion_matrix
+
+# be able to accept JSON data, turn it into a csv file
+def json_to_csv():
+    # Opening JSON file and loading the data 
+    # into the variable data 
+    with open('data.json') as json_file: 
+        data = json.load(json_file) 
+    
+    employee_data = data['emp_details'] 
+    
+    # now we will open a file for writing 
+    data_file = open('data_file.csv', 'w') 
+    
+    # create the csv writer object 
+    csv_writer = csv.writer(data_file) 
+    
+    # Counter variable used for writing  
+    # headers to the CSV file 
+    count = 0
+    
+    for emp in employee_data: 
+        if count == 0: 
+    
+            # Writing headers of CSV file 
+            header = emp.keys() 
+            csv_writer.writerow(header) 
+            count += 1
+    
+        # Writing data of CSV file 
+        csv_writer.writerow(emp.values()) 
+    
+    data_file.close() 
